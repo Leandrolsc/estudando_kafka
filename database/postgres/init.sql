4008ca6b3f2ea8ed4b8b0dce3be543486f5b59de
Leandro Victor Silva <leandrovictor0103@gmail.com>
2025-11-06 21:48:41 -0300

    Ativando kafka connect

--- database/postgres/init.sql
+++ database/postgres/init.sql
@@ -10,6 +10,16 @@ CREATE TABLE IF NOT EXISTS "events_kafka" (
     "dataAlteracao" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
+
+-- Tabela de Eventos Arquivos
+CREATE TABLE IF NOT EXISTS "events_kafka_archives" (
+    "id" SERIAL PRIMARY KEY,
+    "game" TEXT,
+    "eventType" TEXT,
+    "event" TEXT,
+    "dataAlteracao" TIMESTAMPZ
+);
+
 -- Mensagem de sucesso (opcional, aparecerá nos logs do Docker)
 \echo 'Tabelas criadas e usuário master garantido com sucesso!'
 
